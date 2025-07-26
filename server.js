7a7ab8551c17616fe41561951621f6787d52922b
boltxprince <binjweaasha@gmail.com>
2025-07-26 20:51:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const Student = require('./models/Student');
 const Internship = require('./models/Internship');
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
